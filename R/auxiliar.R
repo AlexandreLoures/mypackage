575bcd899e176f93385bdee0c71271907dac5372
Alexandre Loures <arloures@protonmail.com>
2022-07-01 21:49:48 -0300

    first commit

---
+++ R/auxiliar.R
@@ -0,0 +1,19 @@
+soma <- function (a, b) {
+  calculo <- a + b
+  return (calculo)
+}
+
+subtra <- function (a, b) {
+  calculo <- a - b
+  return (calculo)
+}
+
+multi <- function (a, b) {
+  calculo <- a * b
+  return (calculo)
+}
+
+divisi <- function (a, b) {
+  calculo <- a / b
+  return (calculo)
+}
