e62ab6c1137a2cc972a53aff622d063e1e5ed6d1
Alexandre Loures <arloures@protonmail.com>
2022-07-03 21:29:02 -0300

    Add RD files

--- R/auxiliar.R
+++ R/auxiliar.R
@@ -2,22 +2,21 @@
 #'
 #' @examples
 #' soma (2, 2)
-#' @export
-
 soma <- function (a, b) {
   calculo <- a + b
   return (calculo)
 }
+#' subtra (4, 2)
 subtra <- function (a, b) {
   calculo <- a - b
   return (calculo)
 }
-
+#' multi (3, 3)
 multi <- function (a, b) {
   calculo <- a * b
   return (calculo)
 }
-
+#' divisi (8, 4)
 divisi <- function (a, b) {
   calculo <- a / b
   return (calculo)
